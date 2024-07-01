4671c8aeccc642c20232e4c2e202c70d71050c9b
Felipe Ramirez <framirez@bwl.com.mx>
2024-06-30 21:08:27 -0600

    refactor: :art: add prod url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
-  url: 'http://192.168.101.100:3000/',
-  apiKey: '6850ce2e80fa3d46b30d0ce3f8bf1a3e',
+  url: 'http://nestpos-env.eba-xbypm5mf.us-east-1.elasticbeanstalk.com/',
   production: true,
 };
