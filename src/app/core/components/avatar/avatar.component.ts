575049e3805d1877fb5349f9878f0ce6b2872437
Felipe Ramirez <feldjesus@gmail.com>
2024-08-20 22:34:14 -0600

    feat: :art: change item styles (bigger)

--- src/app/core/components/avatar/avatar.component.ts
+++ src/app/core/components/avatar/avatar.component.ts
@@ -3,17 +3,18 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-avatar',
   template: `<img
-    class="rounded-full mx-auto shadow-mdborder-white transition duration-200 transform hover:scale-110"
+    class="rounded mx-auto shadow-md border-white transition duration-200 transform hover:scale-110 object-cover"
     [ngClass]="{
       'w-32 h-32': size === 'large',
-      'w-12 h-12': size !== 'large',
+      'w-20 h-20 min-h-20 min-w-20': size === 'regular',
+      'w-12 h-12': size === 'small',
       'border-4': !hiddenBorder,
     }"
     [src]="img | images: type | async"
     alt="avatar" />`,
 })
 export class AvatarComponent {
-  @Input() size: 'small' | 'large' = 'small';
+  @Input() size: 'small' | 'large' | 'regular' = 'small';
   @Input() img = '';
   @Input() type: 'user' | 'product' = 'user';
   @Input() hiddenBorder = false;
